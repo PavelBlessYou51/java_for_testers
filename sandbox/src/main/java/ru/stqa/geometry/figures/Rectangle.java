293d0c567250aabcbec98b92803771ae55b172bb
PavelBlessYou51 <prokoshevpavel@mail.ru>
2024-10-20 17:06:50 +0300

    Equals

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -1,5 +1,7 @@
 package ru.stqa.geometry.figures;
 
+import java.util.Objects;
+
 public record Rectangle(double a, double b) {
 
     public Rectangle {
@@ -12,6 +14,20 @@ public record Rectangle(double a, double b) {
     public static void printRectangleArea(double a, double b) {
         var text = String.format("Площадь треугольника со стороной %f и %f = %f", a, b, rectangleArea(a, b));
         System.out.println(text);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o; // приведение типа
+        return (Double.compare(this.a, rectangle.a) == 0 && Double.compare(this.b, rectangle.b) == 0) ||
+                (Double.compare(this.b, rectangle.a) == 0 && Double.compare(this.a, rectangle.b) == 0);
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
     }
 
     private static double rectangleArea(double a, double b) {
