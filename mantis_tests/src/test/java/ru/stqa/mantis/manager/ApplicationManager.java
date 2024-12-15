cd3ea1243e61dd7fce9880edc1e3861cc6442357
PavelBlessYou51 <prokoshevpavel@mail.ru>
2024-12-15 13:48:07 +0300

    Лекция №7

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -0,0 +1,44 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Properties;
+
+public class ApplicationManager {
+
+    private WebDriver driver;
+    private Properties properties;
+    private String string;
+    private SessionHelper sessionHelper;
+
+    public void init(String browser, Properties properties) {
+        this.properties = properties;
+        this.string = browser;
+
+    }
+
+    public WebDriver driver() {
+        if (driver == null) {
+            if ("firefox".equals(string)) {
+                driver = new FirefoxDriver();
+            } else if ("chrome".equals(string)) {
+                driver = new ChromeDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Unknown browser %s", string));
+            }
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get(properties.getProperty("web.baseUrl"));
+            driver.manage().window().fullscreen();
+        }
+        return driver;
+    }
+
+    public SessionHelper session() {
+        if (sessionHelper == null) {
+            sessionHelper = new SessionHelper(this);
+        }
+        return sessionHelper;
+    }
+}
